36bf56477148587e170fe437b8cafe54eaf3a618
bangbangbang12315 <2016202218@ruc.edu.cn>
2020-07-14 21:45:32 +0800

    modify README

--- connectpy.php
+++ connectpy.php
@@ -1,7 +1,5 @@
 <?php
         $GLOBALS = $_POST['chessBox'];
-        $myfile = fopen("testfile.txt", "w");
-        fwrite($myfile, $txt);
         foreach ($GLOBALS as $val) 
         {
             $val = join(",",$val);
